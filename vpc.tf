c3505275808ba6fa2d56619d87443ffb194f4f9a
jcolandr <jcolandr@gmail.com>
2020-03-03 14:05:47 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,12 @@
+
+
+module "vpc" {
+  source  = "app.terraform.io/jdc-sandbox/vpc/aws"
+  version = "2.25.0"
+}
+
+
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "dedicated"
